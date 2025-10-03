24c023b1998d0a9a7f13e1e5a4ac31e72035cbec
Desire Koua <desire0369@gmail.com>
2025-10-03 19:50:44 +0000

    remplace https://jetpredict.vercel.app par https://jet-predict.vercel.ap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "next-themes";
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://jetpredict.vercel.app';
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://jet-predict.vercel.app/login';
 
 export const metadata: Metadata = {
   metadataBase: new URL(APP_URL),
